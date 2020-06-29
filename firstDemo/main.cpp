0694d6545698fda0af680126feb6eb77c5a42ac8
limin <limin215@outlook.com>
2020-06-29 10:03:19 +0800

    commit demo

---
+++ firstDemo/main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: limin
+	> Mail: limin215@outlook.com 
+	> Created Time: Mon 29 Jun 2020 10:00:37 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<< "hello world!"<<endl;
+	return 0;
+}
